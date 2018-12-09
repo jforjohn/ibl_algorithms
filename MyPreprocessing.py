54d50bccfb089857d97da2caceff35101a5d2e3c
jforjohn <giannisotiro@gmail.com>
2018-12-09 17:56:28 +0100

    PREPROCESSING: add raw option

--- MyPreprocessing.py
+++ MyPreprocessing.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 
 
 class MyPreprocessing:
-    def __init__(self):
+    def __init__(self, raw=False):
         pd.set_option('display.max_rows', 500)
         pd.set_option('display.max_columns', 500)
         pd.set_option('display.width', 1000)
+        self.raw = raw
 
 ##
 
@@ -39,37 +40,40 @@ class MyPreprocessing:
 
         # normalize numerical data
         df_num = df.select_dtypes(exclude='object')
-        if df_num.size > 0:
-            df_num = df_num.replace(np.NaN, 0)
-            min_max_scaler = preprocessing.StandardScaler()
-            scaled = min_max_scaler.fit_transform(df_num.values.astype(float))
-            df_normalized = pd.DataFrame(scaled, columns=df_num.columns)
-        else:
-            df_normalized = pd.DataFrame()
-
         df_obj = df.select_dtypes(include='object')
-        #le = preprocessing.LabelEncoder()
-        #encoded = le.fit_transform(new_df)
-        if df_obj.size > 0:
-            #df_encoded = df_obj.apply(lambda x: pd.factorize(x)[0])
-            df_encoded = pd.get_dummies(df_obj)
-            '''
-            # NaN values in categorical columns are 0
-            if nan_cols.size > 0:
-                df_encoded.loc[:, nan_cols] += 1
+        if not self.raw:
+            if df_num.size > 0:
+                df_num = df_num.replace(np.NaN, 0)
+                min_max_scaler = preprocessing.StandardScaler()
+                scaled = min_max_scaler.fit_transform(df_num.values.astype(float))
+                df_normalized = pd.DataFrame(scaled, columns=df_num.columns)
+            else:
+                df_normalized = pd.DataFrame()
 
-            new_df_numvalues = df_encoded.values.astype(float)
-            min_max_scaler = preprocessing.StandardScaler()
-            scaled = min_max_scaler.fit_transform(new_df_numvalues)
-            df_encoded = pd.DataFrame(scaled, columns=df_encoded.columns)
-            #df_encoded = df_encoded.astype('object')
-            df_encoded = df_encoded.astype('float')
-            '''
+            #le = preprocessing.LabelEncoder()
+            #encoded = le.fit_transform(new_df)
+
+            if df_obj.size > 0:
+                #df_encoded = df_obj.apply(lambda x: pd.factorize(x)[0])
+                df_encoded = pd.get_dummies(df_obj)
+                '''
+                # NaN values in categorical columns are 0
+                if nan_cols.size > 0:
+                    df_encoded.loc[:, nan_cols] += 1
+    
+                new_df_numvalues = df_encoded.values.astype(float)
+                min_max_scaler = preprocessing.StandardScaler()
+                scaled = min_max_scaler.fit_transform(new_df_numvalues)
+                df_encoded = pd.DataFrame(scaled, columns=df_encoded.columns)
+                #df_encoded = df_encoded. astype('object')
+                df_encoded = df_encoded.astype('float')
+                '''
+            else:
+                df_encoded = pd.DataFrame()
+
+            self.new_df = pd.concat([df_normalized, pd.DataFrame(df_encoded)], axis=1, sort=False)
         else:
-            df_encoded = pd.DataFrame()
-
-        self.new_df = pd.concat([df_normalized, pd.DataFrame(df_encoded)], axis=1, sort=False)
-
+            self.new_df = pd.concat([df_num, df_obj], axis=1, sort=False)
         #self.new_df = new_df.drop(new_df.columns[len(self.df.columns)-1], axis=1)
 #
 #print(df.select_dtypes(exclude='object'))
